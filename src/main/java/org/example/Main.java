742fb76ff0eec738f8ebd06078431e8252752b2c
antheo <patarroyoninoantheomauricio@gmail.com>
2026-06-25 17:25:57 -0500

    ultimo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,7 +58,7 @@ public class Main {
                             System.out.println("No existe estudiante con este codigo");
                         }
                     }
-                    //buscar();
+                    //buscar2();
                     break;
 
                 case 3:
